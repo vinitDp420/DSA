7aa9b1c6873e5cb2f6ec8181c8b6a0b2f156679d
Vinit Patil <vinit.vdp@gmail.com>
2026-07-13 19:33:42 +0530

    Time: 99 ms (5.06%) | Memory: 20.2 MB (94.45%) - LeetSync

---
+++ 217-contains-duplicate/contains-duplicate.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def containsDuplicate(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        n=len(nums)
+        nums.sort()
+        for i in range(n-1):
+            if nums[i]==nums[i+1]:
+                return True
+        return False
