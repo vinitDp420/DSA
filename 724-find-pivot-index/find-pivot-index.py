8e53ebc0a93116c5ab4a2a55f7eb7fa61d3dd4c5
Vinit Patil <vinit.vdp@gmail.com>
2026-07-18 12:25:59 +0530

    Time: 7 ms (54.42%) | Memory: 13.3 MB (57.33%) - LeetSync

---
+++ 724-find-pivot-index/find-pivot-index.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def pivotIndex(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        left_sum = 0
+        
+        total_sum = sum(nums)
+        
+        for i , num in enumerate(nums):
+            if left_sum == (total_sum - left_sum - num):
+                return i
+            left_sum += num
+        return -1 
+        
+
