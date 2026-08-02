bdfe4e1662264038c1a4003324474c1976202849
Vinit Patil <vinit.vdp@gmail.com>
2026-08-02 20:32:02 +0530

    Time: 89 ms (85.81%) | Memory: 20.6 MB (63.64%) - LeetSync

--- 11-container-with-most-water/container-with-most-water.py
+++ 11-container-with-most-water/container-with-most-water.py
@@ -4,16 +4,16 @@ class Solution(object):
         :type height: List[int]
         :rtype: int
         """
-        n=len(height)
-        left = 0
-        right = n-1
-        max_area=0
+        n = len(height)
+        left = 0 
+        right = n - 1
+        max_con = 0
         while left < right:
-            area = (right-left)*min(height[left],height[right])
-            if area > max_area:
-                max_area = area
+            area = (right - left) * min(height[left] , height[right])
+            if area > max_con:
+                max_con = area
             if height[left] < height[right]:
-                left+=1
+                left += 1
             else:
-                right-=1
-        return max_area
+                right -= 1
+        return max_con
